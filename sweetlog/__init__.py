92604a404775f9fb6708ecdbf7fa80b73157f8ba
Dariush Bahrami <dariushbahrami1993@gmail.com>
2023-12-27 00:39:55 +0330

    feat: add version and description

--- sweetlog/__init__.py
+++ sweetlog/__init__.py
@@ -1,3 +1,7 @@
+"""SweetLog, pythonic logging package"""
+
+__version__ = "0.1.0"
+
 from .datatypes import LoggingLevel, Writable
 from .logger import Logger
 from .streams import FileLoggingStream
